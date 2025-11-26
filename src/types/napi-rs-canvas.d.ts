3733404cac06cd11fefe0635e438a0df6c7912d3
shafwar <naufalshafi15@gmail.com>
2025-11-26 20:47:43 +0700

    bismillah

--- src/types/napi-rs-canvas.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@napi-rs/canvas" {
-  // Minimal type declarations to satisfy TypeScript.
-  // Runtime types come from the library; we only need the shapes we use.
-  export function createCanvas(width: number, height: number): any;
-  export function loadImage(source: any): Promise<any>;
-}
-
-
